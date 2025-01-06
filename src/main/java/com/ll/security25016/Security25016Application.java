ee4bb863ccb734ab788406e283ea54703496b8ab
Gyeonghun Min <riverpower6@naver.com>
2025-01-06 11:18:19 +0900

    settings

---
+++ src/main/java/com/ll/security25016/Security25016Application.java
@@ -0,0 +1,13 @@
+package com.ll.security25016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security25016Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security25016Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/security25016/Security25016ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.security25016;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security25016ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
