3020bdc69b1915cf7ab096ba5edcf926c4fd8b11
Gyeonghun Min <riverpower6@naver.com>
2025-01-06 15:40:13 +0900

    16: add: filter - before security filter

---
+++ src/main/java/com/ll/security250106/global/security/CustomAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package com.ll.security250106.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        System.out.println("CustomAuthenticationFilter.doFilter Internal");
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/ll/security250106/global/security/SecurityConfig.java
+++ src/main/java/com/ll/security250106/global/security/SecurityConfig.java
@@ -1,32 +1,38 @@
 package com.ll.security250106.global.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
     SecurityFilterChain baseSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/h2-console/**")
-                                .permitAll()
+                                authorizeRequests
+                                        .requestMatchers("/h2-console/**")
+                                        .permitAll()
 //                                .requestMatchers("/h2-console/login.do")    // 상위 룰 우선이기 때문에 의미 없음
 //                                .authenticated()                              // 상위 룰 우선이기 때문에 의미 없음
-                                .requestMatchers(HttpMethod.GET,
-                                        "/api/*/posts",
-                                        "/api/*/posts/{id:\\d+}",
-                                        "/api/*/posts/{postId:\\d+}/comments")
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated()
+                                        .requestMatchers(HttpMethod.GET,
+                                                "/api/*/posts",
+                                                "/api/*/posts/{id:\\d+}",
+                                                "/api/*/posts/{postId:\\d+}/comments")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated()
                 )
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
